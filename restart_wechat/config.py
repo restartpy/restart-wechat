081b06c6dc29a73988e1a6b8e59136a875b4a9b1
RussellLuo <luopeng.he@gmail.com>
2015-12-13 17:55:32 +0800

    Add support for encryption mode

---
+++ restart_wechat/config.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+from easyconfig import Config
+
+
+config = Config({
+    'TOKEN': '',
+    'ENCODING_AES_KEY': '',
+    'APP_ID': ''
+})
--- restart_wechat/wechat.py
+++ restart_wechat/wechat.py
@@ -2,29 +2,29 @@
 
 from restart.parsers import Parser
 from restart.renderers import Renderer
-from restart.utils import locked_cached_classproperty
-from restart.exceptions import Forbidden, NotImplemented
+from restart.exceptions import Forbidden, NotImplemented, BadRequest
 from wechatpy import parse_message, create_reply
+from wechatpy.crypto import WeChatCrypto
 from wechatpy.utils import check_signature
-from wechatpy.exceptions import InvalidSignatureException
+from wechatpy.exceptions import (
+    InvalidSignatureException,
+    InvalidAppIdException
+)
 
 from .interface import Interface
+from .config import config
 
 
 class CheckSignatureMiddleware(object):
     """The middleware class for checking signature."""
 
-    def __init__(self, token):
-        self.token = token
-
     def process_request(self, request):
-        args = request.args
-        signature = args.get('signature', '')
-        timestamp = args.get('timestamp', '')
-        nonce = args.get('nonce', '')
+        signature = request.args.get('signature', '')
+        timestamp = request.args.get('timestamp', '')
+        nonce = request.args.get('nonce', '')
 
         try:
-            check_signature(self.token, signature, timestamp, nonce)
+            check_signature(config.TOKEN, signature, timestamp, nonce)
         except InvalidSignatureException:
             raise Forbidden()
 
@@ -34,8 +34,25 @@ class XMLParser(Parser):
 
     content_type = 'text/xml'
 
-    def parse(self, stream, content_type, content_length):
+    def parse(self, stream, content_type, content_length, context=None):
         data = stream.read()
+
+        request = context['request']
+        encrypt_type = request.args.get('encrypt_type', 'raw')
+        msg_signature = request.args.get('msg_signature', '')
+        timestamp = request.args.get('timestamp', '')
+        nonce = request.args.get('nonce', '')
+
+        # In encryption mode
+        if encrypt_type != 'raw':
+            crypto = WeChatCrypto(config.TOKEN, config.ENCODING_AES_KEY,
+                                  config.APP_ID)
+            try:
+                data = crypto.decrypt_message(data, msg_signature,
+                                              timestamp, nonce)
+            except (InvalidSignatureException, InvalidAppIdException):
+                raise BadRequest()
+
         msg = parse_message(data)
         return msg
 
@@ -46,15 +63,28 @@ class XMLRenderer(Renderer):
     content_type = 'text/xml'
     format_suffix = 'xml'
 
-    def render(self, data):
+    def render(self, data, context=None):
+        request = context['request']
+        encrypt_type = request.args.get('encrypt_type', 'raw')
+        timestamp = request.args.get('timestamp', '')
+        nonce = request.args.get('nonce', '')
+
+        # In encryption mode
+        if encrypt_type != 'raw':
+            crypto = WeChatCrypto(config.TOKEN, config.ENCODING_AES_KEY,
+                                  config.APP_ID)
+            data = crypto.encrypt_message(data, nonce, timestamp)
+
         return data
 
 
 class Wechat(Interface):
-    """The class that represents a Webchat interface."""
+    """The class that represents a Wechat interface."""
 
-    #: The token used to check signature
+    #: The Wechat configuration
     token = None
+    encoding_aes_key = None
+    app_id = None
 
     #: The message handler mapping
     handlers = {
@@ -73,9 +103,22 @@ class Wechat(Interface):
     #: The renderer classes used for rendering response data.
     renderer_classes = (XMLRenderer,)
 
-    @locked_cached_classproperty(name='_middlewares')
-    def middlewares(cls):
-        return (CheckSignatureMiddleware(cls.token),)
+    #: The resource-level middleware classes
+    middleware_classes = (CheckSignatureMiddleware,)
+
+    def __init__(self, action_map):
+        # Assure that the configuration is loaded
+        self.load_config()
+        super(Wechat, self).__init__(action_map)
+
+    @classmethod
+    def load_config(cls):
+        if not getattr(cls, '_has_loaded', False):
+            config.TOKEN = cls.token
+            config.ENCODING_AES_KEY = cls.encoding_aes_key
+            config.APP_ID = cls.app_id
+
+            cls._has_loaded = True
 
     def message_not_handled(self, message):
         raise NotImplemented()
