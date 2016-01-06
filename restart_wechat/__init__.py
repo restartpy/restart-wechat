c666557bcc592987f868610190fd1d274f4f3c6d
RussellLuo <luopeng.he@gmail.com>
2016-01-06 10:46:27 +0800

    Bump version to 0.1.0

--- restart_wechat/__init__.py
+++ restart_wechat/__init__.py
@@ -1,6 +1,6 @@
 """A RESTArt extension for WeChat Media Platform development."""
 
-__version__ = '0.0.0'
+__version__ = '0.1.0'
 __author__ = 'RussellLuo'
 __email__ = 'luopeng.he@gmail.com'
 __license__ = 'MIT'
